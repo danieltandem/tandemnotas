78c9d05f68350933ed687906eb382b2f52f18603
danieltandem <tandem.danielvargas@gmail.es>
2024-04-11 13:33:21 +0200

    mondongo

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-//Api de localstorage
-localStorage.setItem('tarea02','fiesta loca')
-let tarea = localStorage.getItem('tarea02')
-console.log(tarea)
-console.log(localStorage.length)
-
-let tareas = localStorage
-for (let i = 0; i < tareas.length; i++) {
-    let key = localStorage.key(i) // Clave donde esta guardada la info del Est.
-    console.log(key)
-    tarea = localStorage.getItem(key) // Info del Est. en formato JSON
-    console.log(tarea)
+// recoger la informacion 
+function getInfo(){
+    //devuelve el texto
+    console.log('texto ingresado')
+    return 'texto ingresado'
 }
-//localStorage.clear()
+//Almacenarla en localstorage
+function setLocalInfo(){
+    //guardaria la informacion en localStorage
+    return true;
+
+}
+//leerla y sacarla por pantalla
+function showInfo(){
+    //leerla y mostrarla por pantalla
+    //sino hay notas muestra todo que no hay nada
+    return true;
+}
+//limpiarla
+function cleanInfo(){
+    //limpiar el local storage y limpiar la pantalla
+}
